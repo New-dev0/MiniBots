8d6a6d4b76fd87455481cde56445f0403b882132
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-01-21 18:17:37 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,27 +14,15 @@ Delevents = Client('Delete Events Bot',
                    bot_token=var.BOT_TOKEN
                    )
 
-@Delevents.on_message(filters.group)
+@Delevents.on_message(filters.service)
 async def main(client , message):
-    bot = message._client
-    me = await bot.get_me()
-    try:
-        if message.service==True:
-            whato = await message.chat.get_member(me.id)
-            if whato.status=="administrator" and whato.can_delete_messages==True:
                 return await message.delete()
-    except Exception as e:
-        print(str(e) + message.chat.id)
+
     
 
-@Delevents.on_message(filters.new_chat_members & filters.group)
+@Delevents.on_message(filters.new_chat_members & filters.me)
 async def greet(client,message):
-    bot = message._client
-    getme = await bot.get_me()
-    if message.new_chat_members:
-        for i in message.new_chat_members:
-            if i.id==getme.id:
-                return await message.reply_text('**Thanks for Adding me Here !\n\nMake me Admin with right of Deleting Messages.\n\n@Futurecodes**')
+    await message.reply_text('**Thanks for Adding me Here !\n\nMake me Admin with right of Deleting Messages.\n\n@Futurecodes**')
 
 
 @Delevents.on_message(filters.private & filters.command('start'))
